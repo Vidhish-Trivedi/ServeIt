86bc19503a4e191fea0d17f7ae22bac6a01e7eaa
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-08-27 10:38:00 +0530

    Update hashtable to allow resizing

--- header/hashtable.h
+++ header/hashtable.h
@@ -1,8 +1,12 @@
 #include <stdint.h>
+#include <string>
 #include "./utils.h"
 
 #ifndef HASH_H
 #define HASH_H
+
+const size_t k_resizing_work = 128;
+const size_t k_max_load_factor = 8;
 
 // Node for hashtable.
 struct HNode
@@ -18,10 +22,30 @@ struct HTab {
     size_t size = 0;
 };
 
+// Hashtable interface, 2 hashtables for progressive resizing.
+struct HMap {
+    HTab ht1;
+    HTab ht2;
+    size_t resizing_pos = 0;
+};
+
+// Struct for data payload.
+struct entry {
+    struct HNode node;
+    std::string key;
+    std::string val;
+};
+
+
+void hm_help_resize(HMap *hmap);
+void hm_start_resize(HMap *hmap);
 
 void h_init(HTab *htab, size_t n);
 void h_insert(HTab *htab, HNode *node);
 HNode **h_lookup(HTab *htab, HNode *key, bool (*cmp)(HNode*, HNode*));
 HNode *h_detach(HTab *htab, HNode **from);
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
+void hm_insert(HMap *hmap, HNode *node);
+HNode *hm_del(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
 
 #endif
--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -44,3 +44,95 @@ HNode *h_detach(HTab *htab, HNode **from) {
     htab->size--;
     return(node);
 }
+
+// Function to help resize HMap.
+void hm_help_resize(HMap *hmap) {
+    if(hmap->ht2.table == NULL) {
+        return;
+    }
+
+    size_t n_work = 0;
+    while(n_work < k_resizing_work && hmap->ht2.size > 0) {
+        // Find nodes in ht2 and move them to ht1.
+        HNode **from = &hmap->ht2.table[hmap->resizing_pos];
+        
+        // Not Found.
+        if(!*from) {
+            hmap->resizing_pos++;
+            continue;
+        }
+
+        // Found.
+        h_insert(&hmap->ht1, h_detach(&hmap->ht2, from));
+        n_work++;
+    }
+
+    // Cleanup.
+    if(hmap->ht2.size == 0) {
+        free(hmap->ht2.table);
+        hmap->ht2 = HTab{};
+    }
+}
+
+
+// The lookup with resizing.
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *)) {
+    hm_help_resize(hmap);
+    
+    // look in first table.
+    HNode **from = h_lookup(&hmap->ht1, key, cmp);
+    
+    // look in second table.
+    if(!from) {
+        from = h_lookup(&hmap->ht2, key, cmp);
+    }
+
+    return (from ? *from : NULL);
+}
+
+// Function to initialize a larger hashtable for resizing.
+void hm_start_resize(HMap *hmap) {
+    assert(hmap->ht2.table == NULL);
+    // swap current table.
+    hmap->ht2 = hmap->ht1;
+
+    // create a bigger table.
+    h_init(&hmap->ht1, (hmap->ht1.mask + 1)*2);
+    hmap->resizing_pos = 0;
+}
+
+// Insertion with resizing, will trigger resizing if the table becomes too full.
+void hm_insert(HMap *hmap, HNode *node) {
+    if(!hmap->ht1.table) {
+        h_init(&hmap->ht1, 4);
+    }
+    h_insert(&hmap->ht1, node);
+
+    if(!hmap->ht2.table) {
+        // Check if resizing is required.
+        size_t load_factor = hmap->ht1.size / (hmap->ht1.mask + 1);
+        if(load_factor >= k_max_load_factor) {
+            hm_start_resize(hmap);
+        }
+    }
+    hm_help_resize(hmap);
+}
+
+// Deleting a key from HMap.
+HNode *hm_del(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *)) {
+    hm_help_resize(hmap);
+    
+    // Look for key in ht1.
+    HNode **from = h_lookup(&hmap->ht1, key, cmp);
+    if(from) {
+        return (h_detach(&hmap->ht1, from));
+    }
+
+    // Look for key in ht2.
+    from = h_lookup(&hmap->ht2, key, cmp);
+    if(from) {
+        return (h_detach(&hmap->ht2, from));
+    }
+
+    return(NULL);
+}
